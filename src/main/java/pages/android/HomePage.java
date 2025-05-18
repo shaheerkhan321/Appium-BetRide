cb7df536f9ec842729bcbb447907ecf84e1758c8
Shaheer Ahmed Khan <154052101+shaheerkhan321@users.noreply.github.com>
2025-05-18 22:03:05 +0500

    fix

--- src/main/java/pages/android/HomePage.java
+++ src/main/java/pages/android/HomePage.java
@@ -32,6 +32,31 @@ private static final String profileButtonXPath = "//android.view.View[@content-d
 private static final String alertMessageXPath = "//android.widget.ImageView[@content-desc=\"ALERT\nAre you sure to want to Logout\"]";
 
 
+
+    private String emailAccessibilityID = "SignInScreen:EmailInput:email";
+    private String passwordAccessibilityID = "SignInScreen:PasswordInput:password";
+    private String signinbtnAccessibilityID = "SignInScreen:SignInButtonText:signinbuttontext";
+    private String ContinuebtnAccessibilityID = "WelcomeScreen:OnContinueButtonText:oncontinuebuttontext";
+    private String NextbtnXpath = "//android.widget.TextView[@text='NEXT']";
+    private String Envscreen_title_xpath = "//android.widget.TextView[@text='Select Environment']";
+    private String Production_title_xpath = "//android.widget.TextView[@text='Production']";
+    private String savebtn_xpath = "//android.widget.TextView[@text='Save']";
+    private String WelcomeTitle = "WelcomeScreen:AppLogo:logo";
+    private String Dashboard_accessbilityID = "NewHomeScreen:SeniorName:seniorname";
+    private String MenubtnXpath = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup";
+    private String signoutbtnAccessibilityID = "SideMenu:LogOutEvent:logout";
+
+
+    private String immediateButtonID = "com.meritnation.store.testingapp:id/btn1";
+    private String flexibleButtonID = "com.meritnation.store.testingapp:id/btn2";
+    private String versionCodeID = "com.meritnation.store.testingapp:id/ver_code";
+    private String versionNameID = "com.meritnation.store.testingapp:id/ver_name";
+    private String resultText = "com.meritnation.store.testingapp:id/txt";
+    private String getTitle = "//android.widget.TextView[@text='The App']";
+    private String TextFieldDemoApp = "messageInput";
+    private String getSaveButton = "//android.view.ViewGroup[@content-desc='messageSaveBtn']/android.widget.TextView";
+
+
     public HomePage(Page page) {
     }
 
@@ -103,4 +128,125 @@ public static AndroidElement getAlertMessageElement() {
     return (AndroidElement) wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(alertMessageXPath)));
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public WebElement getTitle() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(titleAccessibilityID));
+    }
+
+    public WebElement getWelcomeTitle() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(WelcomeTitle));
+    }
+
+    public WebElement getEmailfield() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(emailAccessibilityID));
+    }
+
+    public WebElement getPasswordfield() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(passwordAccessibilityID));
+    }
+
+    public WebElement Signin_btn() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(signinbtnAccessibilityID));
+    }
+
+    public WebElement Continue_btn() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(ContinuebtnAccessibilityID));
+    }
+
+    public WebElement Next_btn() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByXPath(NextbtnXpath));
+    }
+
+    public WebElement Menu_btn() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByXPath(MenubtnXpath));
+    }
+
+    public WebElement Env_screen() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByXPath(Envscreen_title_xpath));
+    }
+
+    public WebElement production_btn() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByXPath(Production_title_xpath));
+    }
+
+    public WebElement save_btn() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByXPath(savebtn_xpath));
+    }
+
+    public WebElement Wellness_dashboard() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(Dashboard_accessbilityID));
+    }
+
+    public WebElement Signout_btn() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(signoutbtnAccessibilityID));
+    }
+
+    public WebElement getVersionCode() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(versionCodeID)));
+    }
+
+    public WebElement getVersionName() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(versionNameID)));
+    }
+
+    public WebElement getButton(int num) {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id("com.meritnation.store.testingapp:id/btn" + num)));
+    }
+
+    public WebElement getImmediateButton() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(immediateButtonID)));
+    }
+
+    public WebElement getFlexibleButton() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(flexibleButtonID)));
+    }
+
+    public WebElement getResult() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(resultText)));
+    }
+
+    public void backButton() {
+        AndroidDriverSetup.getAndroidDriver().navigate().back();
+    }
+
+    public WebElement getTitleDemoApp() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(getTitle)));
+    }
+
+    public WebElement getTabNameDemoApp(String name) {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(name));
+    }
+
+    public WebElement getTextFieldDemoApp() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(TextFieldDemoApp));
+    }
+
+    public WebElement getSaveButtonDemoApp() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(getSaveButton)));
+    }
+
+    public WebElement getEchoedTextDemoApp(String echo) {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(echo));
+    }
+
+    public WebElement getOptionDemoApp(String option) {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(option));
+    }
+
+    public void scrollToElementWithAccessibilityId(String accessibilityId) {
+        String uiAutomatorString = String.format("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().description(\"%s\"))", accessibilityId);
+        AndroidDriverSetup.getAndroidDriver().findElement(MobileBy.AndroidUIAutomator(uiAutomatorString));
+    }
 }
--- src/test/java/step_defination/Android/HomeSteps.java
+++ src/test/java/step_defination/Android/HomeSteps.java
@@ -82,6 +82,23 @@ public void userClicksOnAcceptTermsCheckbox() {
     checkbox.click();
 }
 
+// @When("user enter {string} in otp field")
+// public void userEnterOtp(String otp) throws InterruptedException {
+//     List<AndroidElement> otpFields = getHomePage().getOtpInputFields();
+
+//     assertEquals("Expected 5 OTP input fields", 5, otpFields.size());
+
+//     for (int i = 0; i < otp.length(); i++) {
+//         AndroidElement field = otpFields.get(i);
+//         assertTrue("OTP field " + i + " is not displayed", field.isDisplayed());
+
+//         field.click();  // Focus each field
+//         Thread.sleep(200); // Optional small wait between each input
+//         field.clear();  // Optional
+//         field.setValue(Character.toString(otp.charAt(i))); // Appium's safer alternative
+//     }
+// }
+
 @When("user enter {string} in otp field")
 public void userEnterOtpWithKeyEvents(String otp) throws InterruptedException {
     AndroidElement otpField = HomePage.getOtpInputField();
@@ -114,4 +131,221 @@ public void userShouldSeeAlertMessage() {
     Assert.assertTrue("Alert message not displayed", alert.isDisplayed());
 }
 
+
+
+
+
+    @And("User enters {string} in email field")
+    public void UserEntersonEmailField(String text) {
+        Assert.assertTrue(getHomePage().getEmailfield().isDisplayed());
+        getHomePage().getEmailfield().click();  // Click on the email field
+        getHomePage().getEmailfield().sendKeys(text);  // Enter the provided text
+    }
+
+    @And("User taps on Password field")
+    public void UserTapsonPasswordField() {
+        Assert.assertTrue(getHomePage().getPasswordfield().isDisplayed());
+        getHomePage().getPasswordfield().click();
+    }
+
+    @And("User enters {string} in password field")
+    public void UserEntersonPasswordField(String text) {
+        Assert.assertTrue(getHomePage().getPasswordfield().isDisplayed());
+        getHomePage().getPasswordfield().click();  // Click on the email field
+        getHomePage().getPasswordfield().sendKeys(text);  // Enter the provided text
+    }
+
+    @And("User taps on Signin button")
+    public void UserClicksOnSigninBtn() {
+        Assert.assertTrue(getHomePage().Signin_btn().isDisplayed());
+        getHomePage().Signin_btn().click();
+        getHomePage().Signin_btn().click();
+    }
+
+    @Then("User should see Select Environment screen")
+    public void UserseeEnvScreen() {
+        Assert.assertTrue(getHomePage().Env_screen().isDisplayed());
+    }
+
+    @When("User taps on Production")
+    public void tapsonproduction() {
+        Assert.assertTrue(getHomePage().production_btn().isDisplayed());
+        getHomePage().production_btn().click();  // Click on the email field
+    }
+
+    @And("User taps on Save button")
+    public void tapsonsavebtn() {
+        Assert.assertTrue(getHomePage().save_btn().isDisplayed());
+        getHomePage().save_btn().click();  // Click on the email field
+    }
+
+    @Then("User should see welcome screen")
+    public void UserIsOnWelcomeScreen() {
+        Assert.assertTrue(getHomePage().getWelcomeTitle().isDisplayed());
+    }
+
+    @And("User taps on continue button")
+    public void UserClicksOnContinueBtn() {
+        Assert.assertTrue(getHomePage().Continue_btn().isDisplayed());
+        getHomePage().Continue_btn().click();
+    }
+
+    @And("User taps on next button")
+    public void UserClicksOnnextBtn() {
+        Assert.assertTrue(getHomePage().Next_btn().isDisplayed());
+        getHomePage().Next_btn().click();
+    }
+
+    @Then("User should see Wellness Dashboard")
+    public void UserIsOnWellnessDashboard() {
+        Assert.assertTrue(getHomePage().Wellness_dashboard().isDisplayed());
+    }
+
+    @And("^\\[Home Page\\] Verify the Version Code (.*)$")
+    public void verifyTheCode(String versionCode) {
+        Assert.assertEquals(getHomePage().getVersionCode().getText(), versionCode, "Verify Version Code");
+    }
+
+    @When("User taps on menu button")
+    public void UserClicksOnMenuBtn() {
+        Assert.assertTrue(getHomePage().Menu_btn().isDisplayed());
+        getHomePage().Menu_btn().click();
+    }
+
+    @And("User taps on Signout button")
+    public void UserClicksOnSignoutBtn() {
+        Assert.assertTrue(getHomePage().Signout_btn().isDisplayed());
+        getHomePage().Signout_btn().click();
+    }
+//
+//    @And("^\\[Home Page\\] Verify the Version Name (.*)$")
+//    public void verifyTheName(String versionName) {
+//        Assert.assertEquals(getHomePage().getVersionName().getText(), versionName, "Verify Version Name");
+//        getHomePage().getVersionName().isDisplayed();
+//    }
+//
+//    @When("\\[Home Page\\] User tap on Button (.*)$")
+//    public void homePageUserTapOnButtonXxx(int button) {
+//        getHomePage().getButton(button).click();
+//    }
+//
+//    @Then("[Home Page] Verify the Immediate Update button is visible")
+//    public void homePageVerifyTheImmediateUpdateButtonIsVisible() {
+//        getHomePage().getImmediateButton().isDisplayed();
+//    }
+//
+//    @When("[Home Page] User tap on Immediate Update Button")
+//    public void homePageUserTapOnImmediateUpdateButton() {
+//        getHomePage().getImmediateButton().click();
+//    }
+
+//    @Then("[Home Page] Verify the three button options")
+//    public void updatePageVerifyTheThreeButtonOptions(DataTable dataTable) {
+//        List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
+//        for (Map<String, String> itemData : rows) {
+//            if (itemData.containsKey("Button1")) {
+//                $(getHomePage().getButton(1)).shouldHave(Condition.exactText(itemData.get("Button1")));
+//            }
+//            if (itemData.containsKey("Button2")) {
+//                $(getHomePage().getButton(2)).shouldHave(Condition.exactText(itemData.get("Button2")));
+//            }
+//            if (itemData.containsKey("Button3")) {
+//                $(getHomePage().getButton(3)).shouldHave(Condition.exactText(itemData.get("Button3")));
+//            }
+//        }
+//    }
+
+//    @Then("\\[Home Page\\] Verify that Result (.*) is displayed$")
+//    public void homePageVerifyThatResul1IsVisible(String number) {
+//        Assert.assertEquals(getHomePage().getResult().getText(), "RESULT " + number, "Verify Result with Number");
+//        getHomePage().backButton();
+//    }
+//
+//    @When("[Home Page] User tap on Flexible Update Button")
+//    public void homePageUserTapOnFlexibleUpdateButton() {
+//        getHomePage().getFlexibleButton().click();
+//    }
+//
+//    @Then("[Home Page] Verify the Flexible Update button is visible")
+//    public void homePageVerifyTheFlexibleUpdateButtonIsVisible() {
+//        getHomePage().getFlexibleButton().isDisplayed();
+//    }
+
+    @Then("[Home Page] Verify that The App title is displayed on home page")
+    public void homePageVerifyThatTheAppTitleIsDisplayedOnHomePage() {
+        Assert.assertEquals(getHomePage().getTitleDemoApp().getText(), "The Apvp", "Verify Result");
+    }
+
+    @When("^I tap on (.*) tab$")
+    public void iTapOnXxxTab(String name) {
+        getHomePage().getTabNameDemoApp(name).click();
+    }
+
+    @And("I press back button")
+    public void iPressBackButton() {
+        getHomePage().backButton();
+    }
+
+    @When("^I type (.*) in text field on echo box screen$")
+    public void iTypeXxxInTextFieldOnEchoBoxScreen(String text) {
+        getHomePage().getTextFieldDemoApp().sendKeys(text);
+    }
+
+    @When("I tap on save button")
+    public void iTapOnSaveButton() {
+        getHomePage().getSaveButtonDemoApp().click();
+    }
+
+    @Then("^I should see (.*) echoed text on echo box screen$")
+    public void iShouldSeeXxxEchoedTextOnEchoBoxScreen(String echo) {
+        Assert.assertEquals(getHomePage().getEchoedTextDemoApp(echo).getText(), echo, "Verify Result");
+    }
+
+    @When("^I tap on (.*) option$")
+    public void iTapOnXxxOption(String option) {
+        getHomePage().getOptionDemoApp(option).click();
+    }
+
+    @When("^I scroll to (.*) accessibility id$")
+    public void iScrollToXxxAccessibilityId(String id) {
+        getHomePage().scrollToElementWithAccessibilityId(id);
+
+    }
+
+    @And("I accept popup")
+    public void iAcceptPopup() {
+        Alert alert = AndroidDriverSetup.getAndroidDriver().switchTo().alert();
+        alert.accept();
+    }
+
+    @And("I dismiss popup")
+    public void iDismissPopup() {
+        Alert alert = AndroidDriverSetup.getAndroidDriver().switchTo().alert();
+        alert.dismiss();
+    }
+
+    @Then("^I should see popup$")
+    public void iShouldSeePopup() {
+        try {
+            // Attempt to switch to the alert
+            Alert alert = AndroidDriverSetup.getAndroidDriver().switchTo().alert();
+            // If successful, we assume the popup is displayed
+            System.out.println("Popup is displayed with message: " + alert.getText());
+        } catch (NoAlertPresentException e) {
+            // If an exception is caught, it means no popup is present
+            throw new AssertionError("Expected a popup, but none was displayed.");
+        }
+    }
+
+    @And("^I wait (\\d+) seconds$")
+    public void iWaitSeconds(int seconds) {
+        try {
+            // Convert seconds to milliseconds and wait
+            Thread.sleep(seconds * 1000L);
+            System.out.println("Waited " + seconds + " seconds.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Wait was interrupted.");
+        }
+    }
 }
